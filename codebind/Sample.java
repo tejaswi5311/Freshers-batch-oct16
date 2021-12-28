247710d315644c5d883dd657d4bdb71c664701ad
tejaswi5311 <55489585+tejaswi5311@users.noreply.github.com>
2021-12-28 11:33:16 +0530

    Add files via upload

---
+++ codebind/Sample.java
@@ -0,0 +1,17 @@
+package com.codebind;
+
+public class Sample {
+public int add(int a, int b)
+{
+	
+	return a+b;	
+	}
+	public int sub(int a,int b) {
+		return a-b;	
+	}
+		public int divide(int a,int b) {
+		return a/b;
+		}
+
+
+}
