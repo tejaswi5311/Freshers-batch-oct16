8dc5beedcb686a407de0d3480162b0503c05470f
tejaswi5311 <55489585+tejaswi5311@users.noreply.github.com>
2022-01-07 12:56:53 +0530

    Add files via upload

---
+++ calcservice/calcservice/src/main/java/com/calculator/service/calcservice/CalculatorService.java
@@ -0,0 +1,27 @@
+package com.calculator.service.calcservice;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorService {
+	@GetMapping("/add/{x}/{y}")
+	public Response add(@PathVariable int x, @PathVariable int y) {
+		return new Response(x, y, x+y);
+	}
+	@GetMapping("/sub/{x}/{y}")
+	public Response sub(@PathVariable int x, @PathVariable int y) {
+		return new Response(x, y, x-y);
+	}
+	@GetMapping("/multiplication/{x}/{y}")
+	public Response multiplication(@PathVariable int x, @PathVariable int y) {
+		return new Response(x, y, x*y);
+	}
+	@GetMapping("/division/{x}/{y}")
+	public Response division(@PathVariable int x, @PathVariable int y) {
+		return new Response(x, y, x/y);
+	}
+	
+}	
